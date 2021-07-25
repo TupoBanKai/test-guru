b02b68543a70e850fe224fd48742f47bc1ca2c18
TupoBanKai <leagueoflegends.talk@gmail.com>
2021-07-25 22:48:03 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_141750) do
+ActiveRecord::Schema.define(version: 2021_07_24_141923) do
+
+  create_table "answers", force: :cascade do |t|
+    t.boolean "flag", default: true, null: false
+    t.string "correct", null: false
+    t.integer "question_id_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id_id"], name: "index_answers_on_question_id_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "test_id_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id_id"], name: "index_questions_on_test_id_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -26,4 +43,12 @@ ActiveRecord::Schema.define(version: 2021_07_24_141750) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "answers", "question_ids"
+  add_foreign_key "questions", "test_ids"
 end
