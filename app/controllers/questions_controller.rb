c133f0285312be521bf3c5b9e11f68d860b6e8d5
TupoBanKai <leagueoflegends.talk@gmail.com>
2021-09-08 19:46:33 +0300

    HomeWork

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,47 @@
+class QuestionsController < ApplicationController
+  before_action: find_test
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    collection = find_test.questions
+
+    render plain: "#{collection.inspect}"
+  end
+
+  def show
+    find_question
+  end
+
+  def new
+  end
+
+  def create
+    @question = Question.create(question_params)
+
+    render plain: @question.inspect
+  end
+
+  def destroy
+    @question.delete
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body, :test_id)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end
+
+end
---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,5 @@
+class TestsController < ApplicationController
+  def index
+    return 0
+  end
+end
