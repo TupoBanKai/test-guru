b3bab47d45532784dc1362995c06b9c27e7adfa4
TupoBanKai <leagueoflegends.talk@gmail.com>
2021-09-19 19:20:49 +0300

    homework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_date
+    DateTime.now.year
+  end
+
+  def current_link_to_project
+    "<a href='https://github.com/TupoBanKai/test-guru'>Учебный проект в онайн-школе Thinknetica</a>".html_safe
+  end
 end
---
+++ app/helpers/questions_helper.rb
@@ -0,0 +1,6 @@
+module QuestionsHelper
+  def question_header(form)
+    "#{form} quesiton"
+  end
+
+end
