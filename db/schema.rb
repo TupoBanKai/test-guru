a29ada145c635d18501a25632dd71f2c8377a964
TupoBanKai <leagueoflegends.talk@gmail.com>
2021-10-28 20:03:36 +0300

    homework

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_175530) do
+ActiveRecord::Schema.define(version: 2021_10_16_103952) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: true, null: false
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 2021_09_01_175530) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
+  create_table "test_passages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.integer "correct_questions", default: 0
+    t.integer "current_question_id", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 1, null: false
@@ -47,16 +59,6 @@ ActiveRecord::Schema.define(version: 2021_09_01_175530) do
     t.index ["title"], name: "index_tests_on_title", unique: true
   end
 
-  create_table "user_tests", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
-    t.string "result", default: "Not passed", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_user_tests_on_test_id"
-    t.index ["user_id"], name: "index_user_tests_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -65,7 +67,7 @@ ActiveRecord::Schema.define(version: 2021_09_01_175530) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
-  add_foreign_key "user_tests", "tests"
-  add_foreign_key "user_tests", "users"
 end
