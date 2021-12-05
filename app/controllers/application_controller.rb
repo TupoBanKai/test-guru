c149e7be286b61b8f9567bed827791c65d7f975a
TupoBanKai <leagueoflegends.talk@gmail.com>
2021-12-05 23:19:09 +0300

    localized Tests for Admin and User, Device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale
 
   helper_method :current_user,
                 :logged_in?
@@ -7,8 +8,22 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource.admin_check
       tests_path
+      #admin_tests_path
     else
+      #tests_path
       admin_tests_path
     end
+  end
+
+  def default_url_options
+    if I18n.locale != I18n.default_locale
+      { lang: I18n.locale }
+    else
+      {}
+    end
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
