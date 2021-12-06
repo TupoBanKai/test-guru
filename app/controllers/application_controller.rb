4130319852fe12c34b38eafc83d307bd18996186
TupoBanKai <leagueoflegends.talk@gmail.com>
2021-12-06 16:38:02 +0300

    retake 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,21 +6,15 @@ class ApplicationController < ActionController::Base
                 :logged_in?
 
   def after_sign_in_path_for(resource)
-    if resource.admin_check
-      tests_path
-      #admin_tests_path
-    else
-      #tests_path
+    if resource.admin?
       admin_tests_path
+    else
+      tests_path
     end
   end
 
   def default_url_options
-    if I18n.locale != I18n.default_locale
-      { lang: I18n.locale }
-    else
-      {}
-    end
+    (I18n.locale != I18n.default_locale) ? { lang: I18n.locale } : {}
   end
 
   def set_locale
